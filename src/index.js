d3cd1105dd91e346dbcede3624c17610a1d80d6d
Jakub Kania <kubak.15@gmail.com>
2019-01-02 19:00:10 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -17,19 +17,38 @@ function fetchJSON(e) {
       let output = "";
       data.Search.forEach(function(item) {
         output += `<div class="col-sm-4 back-one-row">
-                      <div class="row">
+                      <div class="row ">
                         <div class="col-7"><img  class="img" src="${
                           item.Poster
                         }" alt="" /></div>
                         <div class="col-5">
                           <div>Title: <br /><strong>${item.Title}</strong></div>
                           <div>Year: <br /><strong>${item.Year}</strong></div>
-                          <button class="btn">More details</button>
+                           <a onclick="movieSelected('${
+                             item.imdbID
+                           }')" class="btn" href="#">More Details</a>
                         </div>
                       </div>
                     </div>`;
       });
       document.getElementById("cont").innerHTML = output;
+    })
+    .catch(err => console.log(err));
+}
+
+function movieSelected(id) {
+  sessionStorage.setItem("movieId", id);
+  window.location = "movie.html";
+  return false;
+}
+
+function fetchOneMovie() {
+  let movieId = sessionStorage.getItem("movieId");
+
+  fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${movieId}`)
+    .then(res => res.json())
+    .then(dataOne => {
+      console.log(dataOne);
     })
     .catch(err => console.log(err));
 }
